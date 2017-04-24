20ec537d072ec777626b197d832aca6a97604bc9
Colin Bloom <colinjbloom113@gmail.com>
2017-04-23 19:48:11 -0700

    change tables, change get doctors function, still has bug with displaying it

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -4,9 +4,24 @@ var getDoctors = require('./../js/doctor.js').getDoctors;
 
 var symptoms;
 
+var displayDoctors = function(doctors){
+  console.log(doctors);
+  // doctors.forEach(function(doctor){
+  //   $('#body').text('<tr>'+
+  //                     '<td><img src='+doctor.img+'></img></td>'+
+  //                     '<td>'+doctor.firstName+doctor.lastName+'</td>'+
+  //                     '<td>'+doctor.bio+'</td>'+
+  //                   '</tr>')
+  // });
+}
+
 $(document).ready(function(){
   $('#symptomButton').click(function(event) {
     symptoms = $('#symptoms').val();
-    getDoctors(symptoms);
+    var doctors = getDoctors(symptoms);
+    console.log(doctors);
+    displayDoctors(doctors);
   });
 });
+
+exports.displayDoctors = displayDoctors();
--- js/doctor.js
+++ js/doctor.js
@@ -1,13 +1,32 @@
 var apiKey = require('./../.env').apiKey;
+// var displayDoctors = require('./../js/doctor-interface.js').displayDoctors;
 
 function Doctor() {
-
+  this.firstName;
+  this.lastName;
+  this.gender;
+  this.practices = [];
+  this.img;
+  this.bio;
 }
 
 exports.getDoctors = function(medicalIssue) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
-   .then(function(result) {
-      console.log(result.data[0].practices[0]);
+    .then(function(response) {
+      var doctors = [];
+      response.data.forEach(function(doctor){
+        var newDoctor = new Doctor();
+        newDoctor.firstName = doctor.profile.first_name;
+        newDoctor.lastName = doctor.profile.last_name;
+        newDoctor.gender = doctor.profile.gender;
+        doctor.practices.forEach(function(practice){
+          newDoctor.practices.push(practice)
+        });
+        newDoctor.img = doctor.image_url;
+        newDoctor.bio = doctor.bio;
+        doctors.push(newDoctor);
+      });
+      return doctors;
     })
    .fail(function(error){
       console.log("fail");
